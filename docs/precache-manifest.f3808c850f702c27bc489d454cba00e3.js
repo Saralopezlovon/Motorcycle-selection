1ddc6b1885d04c1c2b4b75bc88b254b9f9f14b0e
Sara <sara@sara.lopez>
2020-02-09 18:16:14 +0100

    Update docs

---
+++ docs/precache-manifest.f3808c850f702c27bc489d454cba00e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23478ba75f7e3749fa59e3b7720cce7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d48cd1f5d4a4daed6033",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "25b55037aa103f92171e",
+    "url": "./static/js/2.ba627d18.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.ba627d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48cd1f5d4a4daed6033",
+    "url": "./static/js/main.f66444cf.chunk.js"
+  },
+  {
+    "revision": "873befb6a8bd974a5d71",
+    "url": "./static/js/runtime-main.953bed08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);

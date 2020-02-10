aa9fb5cad7f68e94d026da903c51e4fc48c7c37b
Sara <sara@sara.lopez>
2020-02-10 02:44:37 +0100

    Add link to detail

--- src/components/MotorcycleCard.js
+++ src/components/MotorcycleCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import "../stylesheets/motorcyclecard.scss";
+import { Link } from 'react-router-dom';
 
 const MotorcycleCard=(props)=> {
     return (
-        <div>{props.motorcycle.nombre}</div>
-        
+        <li> 
+          <Link to={`/motorcycles/${props.motorcycles.id}`}>     
+            <img src={props.motorcycle.imagen} alt={props.motorcycle.nombre}></img>
+            <ul>           
+              <li>{props.motorcycle.marca}</li>
+              <li>{props.motorcycle.nombre}</li>
+              <li>{props.motorcycle.año}</li>
+            </ul>
+          </Link>  
 
-        
+      </li>
+                
     );
     };
 

baed65e4fdffa2b0f187cab107a5a0218694121d
Sara <sara@sara.lopez>
2020-02-22 23:52:56 +0100

    Add background header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "../stylesheets/header.scss";
+import brillo from "../images/brillo.png";
 
 function Header() {
     return (
         <div className="header">
             <span className="header_title"> - Wellcome to the jungle - </span>    
+            
         </div>
 
     );
